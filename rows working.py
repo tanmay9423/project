bcf066ff233d7aab8bff8c0ea4fbb60b540b8117
Tanmay Shravge <51810573+tanmay9423@users.noreply.github.com>
2019-10-07 13:26:33 +0530

    Add files via upload

---
+++ rows working.py
@@ -0,0 +1,22 @@
+import csv
+import numpy as np
+i=0
+j=0
+mean=0
+meanf=[0,0,0,0,0,0,0,0]
+with open("144.csv", "r") as f:
+    reader = csv.reader(f, delimiter="\t")
+    print(enumerate(reader))
+    for i, line in enumerate(reader):
+        mean=0
+        print('line[{}] = {}'.format(i, line))
+        line[0] = eval('[' + line[0] + ']')
+        for j in range(1,7):
+            mean=mean+line[0][j]
+            #print(line[0][])
+        meanf[i]=mean/6
+        print(meanf[i])
+print(meanf)
+#while i!=6:
+    #print(line0[i])
+    #i=i+1
