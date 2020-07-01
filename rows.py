f11d1af1f21c7742431b7da2b2a1e73571ed4731
Tanmay Shravge <51810573+tanmay9423@users.noreply.github.com>
2020-07-01 20:36:40 +0530

    Delete rows.py

--- rows.py
+++
@@ -1,8 +0,0 @@
-import csv
-import numpy
-
-with open("144.csv", "r") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for i, line in enumerate(reader):
-        print('line[{}] = {}'.format(i, line))
-
